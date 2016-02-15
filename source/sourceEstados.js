4c9c5e7d47ae5664048f3212f52d1ddd9ff32aa4
Yuri Marinho Alcântara <yuri.maralca@outlook.com>
2016-02-15 20:58:24 -0300

    -

--- source/sourceEstados.js
+++ source/sourceEstados.js
@@ -1,1 +1,52 @@
-var rawDataEstados = {"alt":{"coluna":[{"url":["http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://...","http://..."],"dadosFormatados":["AC - Acre","AL - Alagoas","AM - Amazonas","AP - Amapá","BA - Bahia","CE - Ceará","DF - Distrito Federal","ES - Espírito Santo","GO - Goiás","MA - Maranhão","MG - Minas Gerais","MS - Mato Grosso do Sul","MT - Mato Grosso","PA - Pará","PB - Paraíba","PE - Pernambuco","PI - Piauí","PR - Paraná","RJ - Rio de Janeiro","RN - Rio Grande do Norte","RO - Rondônia","RR - Roraima","RS - Rio Grande do Sul","SC - Santa Catarina","SE - Sergipe","SP - São Paulo","TO - Tocantins"],"dados":["AC","AL","AM","AP","BA","CE","DF","ES","GO","MA","MG","MS","MT","PA","PB","PE","PI","PR","RJ","RN","RO","RR","RS","SC","SE","SP","TO"],"titulo":"Estado","tipo":"geografica"},{"dadosFormatados":["53","202","277","8","2.604","941","763","994","902","197","5.463","269","248","121","634","1.477","38","2.362","4.986","230","40","30","1.376","588","285","13.436","107"],"dados":[53,202,277,8,2604,941,763,994,902,197,5463,269,248,121,634,1477,38,2362,4986,230,40,30,1376,588,285,13436,107],"unidade":"mil domicílios","titulo":"Qtd de Domicílios - Tinham Rede Coletora","somaHorizontal":"SH1","somaVertical":true,"tipo":"absoluto"},{"dadosFormatados":["76","354","461","69","674","299","149","146","444","724","235","68","239","1.018","231","507","754","557","566","481","187","108","2.044","1.443","81","795","112"],"dados":[76,354,461,69,674,299,149,146,444,724,235,68,239,1018,231,507,754,557,566,481,187,108,2044,1443,81,795,112],"unidade":"mil domicílios","titulo":"Qtd de Domicílios - Tinham Fossa Séptica","somaHorizontal":"SH1","somaVertical":true,"tipo":"absoluto"},{"dadosFormatados":["27","44","105","28","184","76","1","80","5","51","338","1","5","94","49","190","4","16","319","14","6","1","102","33","12","186","5"],"dados":[27,44,105,28,184,76,1,80,5,51,338,1,5,94,49,190,4,16,319,14,6,1,102,33,12,186,5],"unidade":"mil domicílios","titulo":"Qtd de Domicílios - Tinham Outro","somaHorizontal":"SH1","somaVertical":true,"tipo":"absoluto"},{"dadosFormatados":["30","48","71","3","198","118","1","2","6","297","54","3","14","128","40","110","133","20","20","18","8","3","22","5","18","28","32"],"dados":[30,48,71,3,198,118,1,2,6,297,54,3,14,128,40,110,133,20,20,18,8,3,22,5,18,28,32],"unidade":"mil domicílios","titulo":"Qtd de Domicílios - Não Tinham","somaHorizontal":"SH1","somaVertical":true,"tipo":"absoluto"},{"dadosFormatados":["222","993","1.038","200","4.973","2.768","936","1.321","2.217","1.917","6.965","886","1.094","2.263","1.222","2.961","963","3.819","5.989","1.071","575","155","4.074","2.381","694","14.855","484"],"dados":[222,993,1038,200,4973,2768,936,1321,2217,1917,6965,886,1094,2263,1222,2961,963,3819,5989,1071,575,155,4074,2381,694,14855,484],"unidade":"mil domicílios","titulo":"Qtd Total de Domicílios","somaHorizontal":null,"somaVertical":true,"tipo":"absoluto"}],"titulo":"Teste"}};
+var rawDataEstados = {
+    "alt": {
+        "coluna": [{
+            "url": ["http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://...", "http://..."],
+            "dadosFormatados": ["AC - Acre", "AL - Alagoas", "AM - Amazonas", "AP - Amapá", "BA - Bahia", "CE - Ceará", "DF - Distrito Federal", "ES - Espírito Santo", "GO - Goiás", "MA - Maranhão", "MG - Minas Gerais", "MS - Mato Grosso do Sul", "MT - Mato Grosso", "PA - Pará", "PB - Paraíba", "PE - Pernambuco", "PI - Piauí", "PR - Paraná", "RJ - Rio de Janeiro", "RN - Rio Grande do Norte", "RO - Rondônia", "RR - Roraima", "RS - Rio Grande do Sul", "SC - Santa Catarina", "SE - Sergipe", "SP - São Paulo", "TO - Tocantins"],
+            "dados": ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO"],
+            "titulo": "Estado",
+            "tipo": "geografica"
+        }, {
+            "dadosFormatados": ["53", "202", "277", "8", "2.604", "941", "763", "994", "902", "197", "5.463", "269", "248", "121", "634", "1.477", "38", "2.362", "4.986", "230", "40", "30", "1.376", "588", "285", "13.436", "107"],
+            "dados": [53, 202, 277, 8, 2604, 941, 763, 994, 902, 197, 5463, 269, 248, 121, 634, 1477, 38, 2362, 4986, 230, 40, 30, 1376, 588, 285, 13436, 107],
+            "unidade": "mil domicílios",
+            "titulo": "Qtd de Domicílios - Tinham Rede Coletora",
+            "somaHorizontal": "SH1",
+            "somaVertical": true,
+            "tipo": "absoluto"
+        }, {
+            "dadosFormatados": ["76", "354", "461", "69", "674", "299", "149", "146", "444", "724", "235", "68", "239", "1.018", "231", "507", "754", "557", "566", "481", "187", "108", "2.044", "1.443", "81", "795", "112"],
+            "dados": [76, 354, 461, 69, 674, 299, 149, 146, 444, 724, 235, 68, 239, 1018, 231, 507, 754, 557, 566, 481, 187, 108, 2044, 1443, 81, 795, 112],
+            "unidade": "mil domicílios",
+            "titulo": "Qtd de Domicílios - Tinham Fossa Séptica",
+            "somaHorizontal": "SH1",
+            "somaVertical": true,
+            "tipo": "absoluto"
+        }, {
+            "dadosFormatados": ["27", "44", "105", "28", "184", "76", "1", "80", "5", "51", "338", "1", "5", "94", "49", "190", "4", "16", "319", "14", "6", "1", "102", "33", "12", "186", "5"],
+            "dados": [27, 44, 105, 28, 184, 76, 1, 80, 5, 51, 338, 1, 5, 94, 49, 190, 4, 16, 319, 14, 6, 1, 102, 33, 12, 186, 5],
+            "unidade": "mil domicílios",
+            "titulo": "Qtd de Domicílios - Tinham Outro",
+            "somaHorizontal": "SH1",
+            "somaVertical": true,
+            "tipo": "absoluto"
+        }, {
+            "dadosFormatados": ["30", "48", "71", "3", "198", "118", "1", "2", "6", "297", "54", "3", "14", "128", "40", "110", "133", "20", "20", "18", "8", "3", "22", "5", "18", "28", "32"],
+            "dados": [30, 48, 71, 3, 198, 118, 1, 2, 6, 297, 54, 3, 14, 128, 40, 110, 133, 20, 20, 18, 8, 3, 22, 5, 18, 28, 32],
+            "unidade": "mil domicílios",
+            "titulo": "Qtd de Domicílios - Não Tinham",
+            "somaHorizontal": "SH1",
+            "somaVertical": true,
+            "tipo": "absoluto"
+        }, {
+            "dadosFormatados": ["222", "993", "1.038", "200", "4.973", "2.768", "936", "1.321", "2.217", "1.917", "6.965", "886", "1.094", "2.263", "1.222", "2.961", "963", "3.819", "5.989", "1.071", "575", "155", "4.074", "2.381", "694", "14.855", "484"],
+            "dados": [222, 993, 1038, 200, 4973, 2768, 936, 1321, 2217, 1917, 6965, 886, 1094, 2263, 1222, 2961, 963, 3819, 5989, 1071, 575, 155, 4074, 2381, 694, 14855, 484],
+            "unidade": "mil domicílios",
+            "titulo": "Qtd Total de Domicílios",
+            "somaHorizontal": null,
+            "somaVertical": true,
+            "tipo": "absoluto"
+        }],
+        "titulo": "Teste"
+    }
+};
